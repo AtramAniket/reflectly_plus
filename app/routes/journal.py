6f7c211c9e3e266a535f67ea8c00d31b1c8f0fa0
AtramAniket <aniket_atram@live.in>
2026-03-30 15:00:32 +0530

    added edit and delete functionality

--- app/routes/journal.py
+++ app/routes/journal.py
@@ -1,11 +1,11 @@
 from app.extensions import db
 from app.models.journal import JournalEntry
 from flask_login import current_user, login_required
-from flask import Blueprint, url_for, render_template, request, redirect, flash
+from flask import Blueprint, url_for, render_template, request, redirect, flash, abort
 
 journal = Blueprint('journal', __name__)
 
-@journal.route('/create_new_entry', methods=['GET', 'POST'])
+@journal.route('/journal/create_new_entry', methods=['GET', 'POST'])
 @login_required
 def create_new_entry():
 	if request.method == 'POST':
@@ -28,3 +28,46 @@ def create_new_entry():
 		return redirect(url_for('main.dashboard'))
 
 	return render_template('create_entry.html')
+
+@journal.route('/journal/edit/<int:entry_id>', methods=['GET', 'POST'])
+@login_required
+def edit_entry(entry_id):
+
+	entry = JournalEntry.query.get_or_404(entry_id) 
+
+	if entry.user_id != current_user.id:
+		abort(403)
+
+
+	if request.method == 'POST':
+
+		entry.title = request.form.get('title')
+		entry.content = request.form.get('content')
+		entry.mood_score = request.form.get('mood_score') or 0
+
+		db.session.commit()
+
+		flash('Entry updated successfully', 'success')
+		return redirect(url_for('main.dashboard'))
+
+	return render_template('edit_entry.html', journal_entry = entry)
+
+@journal.route('/journal/delete_article/<int:entry_id>', methods=['POST'])
+@login_required
+def delete_entry(entry_id):
+
+	entry = JournalEntry.query.get_or_404(entry_id) 
+
+	if entry.user_id != current_user.id:
+		abort(403)
+
+
+	if request.method == 'POST':
+
+		db.session.delete(entry)
+		db.session.commit()
+
+		flash('Entry deleted successfully', 'success')
+		return redirect(url_for('main.dashboard'))
+
+	return render_template('edit_entry.html', journal_entry = entry)
