48dc0ad8c640d45a3176d784bc5ec5da649cfae8
AtramAniket <aniket_atram@live.in>
2026-03-31 13:24:35 +0530

    added analytics to calculate average mood score

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from flask_login import current_user, login_required
 from flask import Blueprint, render_template, redirect, url_for
 
@@ -18,4 +19,21 @@ def dashboard():
     
     entries = sorted(current_user.entries, key=lambda x: x.created_at, reverse=True)
 
-    return render_template('dashboard.html', email=current_user.email, journal_entries=entries)
+    total_entries = len(entries)
+
+    mood_values = [e.mood_score for e in entries if e.mood_score is not None]
+
+    avg_mood_score = round(sum(mood_values) / len(mood_values), 2) if mood_values else None
+
+    one_week_ago = datetime.utcnow() - timedelta(days=7)
+
+    entries_this_week = [e for e in entries if e.created_at and e.created_at >= one_week_ago]
+
+
+    return render_template('dashboard.html', 
+        email=current_user.email, 
+        journal_entries=entries,
+        average_mood_score = avg_mood_score,
+        total_entries=total_entries,
+        entries_this_week = len(entries_this_week)
+        )
