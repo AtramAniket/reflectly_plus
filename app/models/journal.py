633e46ec5e50d03d9a0058d204c716c4da12226b
AtramAniket <aniket_atram@live.in>
2026-04-07 13:38:10 +0530

    added entry type column to JournalEntry model

--- app/models/journal.py
+++ app/models/journal.py
@@ -11,6 +11,8 @@ class JournalEntry(UserMixin, db.Model):
 
 	id: Mapped[int] =  mapped_column(Integer, primary_key=True)
 
+	entry_type: Mapped[str] = mapped_column(String(50), nullable=False, default='simple')
+
 	title: Mapped[str] = mapped_column(String(250), nullable=False)
 
 	content: Mapped[str] = mapped_column(Text, nullable=False)
