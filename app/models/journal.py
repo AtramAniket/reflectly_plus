e53a221e1aaa845e3d048b4d0472408b67ebda9b
AtramAniket <aniket_atram@live.in>
2026-04-07 13:56:49 +0530

    added migrations to model

--- app/models/journal.py
+++ app/models/journal.py
@@ -12,7 +12,7 @@ class JournalEntry(UserMixin, db.Model):
 
 	id: Mapped[int] =  mapped_column(Integer, primary_key=True)
 
-	entry_type: Mapped[str] = mapped_column(String(50), default='simple')
+	entry_type: Mapped[str] = mapped_column(String(50), default='simple', nullable=True)
 
 	title: Mapped[str] = mapped_column(String(250), nullable=False)
 
