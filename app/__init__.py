ac3e1d5d6e8ed793d5a69e482620ee6274cfcadd
AtramAniket <aniket_atram@live.in>
2026-04-10 11:28:02 +0530

    added mood checklist model to feature store

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ def create_app():
     from .models.habit import Habit
     from .models.habit_log import HabitLog
     from .models.journal import JournalEntry
+    from .models.mood_checklist import MoodChecklistResult
     from .models.journal_ai_analysis import JournalAIAnalysis
     
     app.register_blueprint(main)
