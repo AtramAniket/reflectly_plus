821198b7dd19beed517b6674714f825737634bd0
AtramAniket <aniket_atram@live.in>
2026-04-15 13:56:01 +0530

    added blueprint for insights routes

--- app/routes/insight.py
+++ app/routes/insight.py
@@ -3,9 +3,9 @@ from flask_login import current_user, login_required
 
 from app.helpers.weekly_insights_helper import get_or_generate_weekly_insight
 
-insights_bp = Blueprint("insights", __name__)
+insight = Blueprint("insights", __name__)
 
-@insights_bp.route("/insights")
+@insight.route("/insights")
 @login_required
 def insights():
     insight = get_or_generate_weekly_insight(current_user)
