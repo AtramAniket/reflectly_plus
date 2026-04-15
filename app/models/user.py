69e8963b306d595cf85df74296d0ceb364656dbc
AtramAniket <aniket_atram@live.in>
2026-04-15 14:34:01 +0530

    changed default to server_default to populate date for existing user

--- app/models/user.py
+++ app/models/user.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
 
     # Adding timezone field for user to generate insights
-    timezone: Mapped[str] = mapped_column(String(64), nullable=False, default='UTC')
+    timezone: Mapped[str] = mapped_column(String(64), nullable=False, server_default='UTC')
 
     # Relationship with JournalEntry
     entries: Mapped[list['JournalEntry']] = relationship('JournalEntry', back_populates='user', cascade='all, delete-orphan')
